ca62af458cca6ab6e939db285cea9dfe9ab3192f
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-26 20:37:03 +0000

    Changed extension icon Added external ctrl definition for employees table
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/externalimport_tut/trunk@14317 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -5,8 +5,8 @@ if (!defined ('TYPO3_MODE')) {
 
 t3lib_extMgm::allowTableOnStandardPages('tx_externalimporttut_departments');
 
-$TCA['tx_externalimporttut_departments'] = array (
-	'ctrl' => array (
+$TCA['tx_externalimporttut_departments'] = array(
+	'ctrl' => array(
 		'title'     => 'LLL:EXT:externalimport_tut/locallang_db.xml:tx_externalimporttut_departments',		
 		'label'     => 'name',	
 		'tstamp'    => 'tstamp',
@@ -14,7 +14,7 @@ $TCA['tx_externalimporttut_departments'] = array (
 		'cruser_id' => 'cruser_id',
 		'default_sortby' => 'ORDER BY name',	
 		'delete' => 'deleted',	
-		'enablecolumns' => array (		
+		'enablecolumns' => array(
 			'disabled' => 'hidden',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
@@ -22,8 +22,8 @@ $TCA['tx_externalimporttut_departments'] = array (
 	),
 );
 
-$TCA['tx_externalimporttut_employees'] = array (
-	'ctrl' => array (
+$TCA['tx_externalimporttut_employees'] = array(
+	'ctrl' => array(
 		'title'     => 'LLL:EXT:externalimport_tut/locallang_db.xml:tx_externalimporttut_employees',		
 		'label'     => 'lastname',	
 		'tstamp'    => 'tstamp',
@@ -31,13 +31,45 @@ $TCA['tx_externalimporttut_employees'] = array (
 		'cruser_id' => 'cruser_id',
 		'default_sortby' => 'ORDER BY lastname',	
 		'delete' => 'deleted',	
-		'enablecolumns' => array (		
+		'enablecolumns' => array(
 			'disabled' => 'hidden',	
 			'starttime' => 'starttime',	
 			'endtime' => 'endtime',
 		),
 		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
 		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_externalimporttut_employees.gif',
+		'external' => array(
+			0 => array(
+				'connector' => 'csv',
+				'parameters' => array(
+					'filename' => t3lib_extMgm::extPath($_EXTKEY, 'res/employees.txt'),
+					'delimiter' => ';',
+					'text_qualifier' => '',
+					'skip_rows' => 1,
+					'encoding' => 'utf8'
+				),
+				'data' => 'array',
+				'reference_uid' => 'employee_number',
+				'priority' => 10,
+				'disabledOperations' => '',
+				'description' => 'Import of full employee list'
+			),
+			1 => array(
+				'connector' => 'csv',
+				'parameters' => array(
+					'filename' => t3lib_extMgm::extPath($_EXTKEY, 'res/holidays.txt'),
+					'delimiter' => "\t",
+					'text_qualifier' => '',
+					'skip_rows' => 1,
+					'encoding' => 'latin1'
+				),
+				'data' => 'array',
+				'reference_uid' => 'code',
+				'priority' => 20,
+				'disabledOperations' => 'insert,delete',
+				'description' => 'Import of full employee list'
+			)
+		)
 	),
 );
 ?>
