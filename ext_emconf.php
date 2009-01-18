aa74f450155b69491ddd6b849acc8b9fb45f3d17
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-18 16:50:21 +0000

    Released to TER as version 1.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/externalimport_tut/trunk@15786 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "externalimport_tut"
 #
-# Auto generated 24-11-2008 22:26
+# Auto generated 18-01-2009 17:49
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Francois Suter (Cobweb)',
 	'author_email' => 'typo3@cobweb.ch',
 	'shy' => '',
-	'dependencies' => 'external_import,svnconnector_csv',
+	'dependencies' => 'external_import,svconnector_csv',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'external_import' => '',
@@ -38,10 +38,12 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'devlog' => ''
+			'devlog' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"b1c0";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"7525";s:14:"ext_tables.sql";s:4:"c3e6";s:41:"icon_tx_externalimporttut_departments.gif";s:4:"475a";s:39:"icon_tx_externalimporttut_employees.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"fe1a";s:7:"tca.php";s:4:"59b7";s:19:"doc/wizard_form.dat";s:4:"5b8a";s:20:"doc/wizard_form.html";s:4:"ddf7";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"5853";s:10:"README.txt";s:4:"5a50";s:36:"class.tx_externalimporttut_hooks.php";s:4:"b705";s:46:"class.tx_externalimporttut_transformations.php";s:4:"6209";s:12:"ext_icon.gif";s:4:"da0a";s:17:"ext_localconf.php";s:4:"7dcd";s:14:"ext_tables.php";s:4:"42b0";s:14:"ext_tables.sql";s:4:"fc70";s:41:"icon_tx_externalimporttut_departments.gif";s:4:"0cdd";s:35:"icon_tx_externalimporttut_teams.gif";s:4:"0094";s:16:"locallang_db.xml";s:4:"9c96";s:7:"tca.php";s:4:"f740";s:14:"doc/manual.sxw";s:4:"bfa2";s:19:"res/departments.txt";s:4:"4626";s:17:"res/employees.txt";s:4:"4886";s:16:"res/holidays.txt";s:4:"7b08";s:13:"res/teams.txt";s:4:"ad82";}',
+	'suggests' => array(
+	),
 );
 
 ?>
