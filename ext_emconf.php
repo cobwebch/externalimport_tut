fd2ff1bc4d670e5a588662c232ac3a9b2d1fd662
Francois Suter <fsuter@cobweb.ch>
2018-02-04 22:11:17 +0100

    [TASK] Release version 2.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,15 +19,15 @@ $EM_CONF[$_EXTKEY] = array (
   'createDirs' => '',
   'clearCacheOnLoad' => 1,
   'author_company' => '',
-  'version' => '2.0.1',
+  'version' => '2.1.0',
   'constraints' =>
   array (
     'depends' =>
     array (
       'external_import' => '4.0.0-0.0.0',
-      'svconnector_csv' => '2.0.0-0.0.0',
-      'svconnector_feed' => '2.0.0-0.0.0',
-      'news' => '2.3.0-0.0.0',
+      'svconnector_csv' => '2.2.0-0.0.0',
+      'svconnector_feed' => '2.2.0-0.0.0',
+      'news' => '6.2.0-0.0.0',
       'typo3' => '7.6.0-8.99.99',
     ),
     'conflicts' =>
