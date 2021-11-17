6cf064c32b76097f819135948aa3561bf7342a56
Francois Suter <sutfra@gmail.com>
2021-11-17 21:34:43 +0100

    [BUGFIX] Fix bug with import of news links

--- Configuration/TCA/Overrides/tx_news_domain_model_link.php
+++ Configuration/TCA/Overrides/tx_news_domain_model_link.php
@@ -2,47 +2,55 @@
 
 // Add the external general information
 $GLOBALS['TCA']['tx_news_domain_model_link']['external']['general'] = [
-        0 => [
-                'connector' => 'feed',
-                'parameters' => [
-                        'uri' => 'https://typo3.org/?type=100'
-                ],
-                'data' => 'xml',
-                'nodetype' => 'item',
-                'referenceUid' => 'uri',
-                'enforcePid' => true,
-                'priority' => 210,
-                'group' => 'externalimport_tut',
-                'disabledOperations' => 'delete',
-                'description' => 'Import of typo3.org news related links'
+    0 => [
+        'connector' => 'feed',
+        'parameters' => [
+            'uri' => 'https://typo3.org/?type=100'
         ],
+        'data' => 'xml',
+        'nodetype' => 'item',
+        'referenceUid' => 'uri',
+        'enforcePid' => true,
+        'priority' => 210,
+        'group' => 'externalimport_tut',
+        'disabledOperations' => 'delete',
+        'description' => 'Import of typo3.org news related links'
+    ],
 ];
 // Add the external information for each column
 $GLOBALS['TCA']['tx_news_domain_model_link']['columns']['title']['external'] = [
-        0 => [
-                'field' => 'title'
-        ]
+    0 => [
+        'field' => 'title'
+    ]
 ];
 $GLOBALS['TCA']['tx_news_domain_model_link']['columns']['uri']['external'] = [
-        0 => [
-                'field' => 'link'
+    0 => [
+        'field' => 'link',
+        'transformations' => [
+            10 => [
+                'trim' => true
+            ]
         ]
+    ]
 ];
 $GLOBALS['TCA']['tx_news_domain_model_link']['columns']['parent'] = [
-        'config' => [
-                'type' => 'passthrough',
-        ],
-        'external' => [
-                0 => [
-                        'field' => 'link',
-                        'transformations' => [
-                                10 => [
-                                        'mapping' => [
-                                                'table' => 'tx_news_domain_model_news',
-                                                'referenceField' => 'tx_externalimporttut_externalid'
-                                        ]
-                                ]
-                        ]
+    'config' => [
+        'type' => 'passthrough',
+    ],
+    'external' => [
+        0 => [
+            'field' => 'link',
+            'transformations' => [
+                10 => [
+                    'trim' => true
+                ],
+                20 => [
+                    'mapping' => [
+                        'table' => 'tx_news_domain_model_news',
+                        'referenceField' => 'tx_externalimporttut_externalid'
+                    ]
                 ]
+            ]
         ]
+    ]
 ];
