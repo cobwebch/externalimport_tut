692f9a47a49f1815d168a6e60da0aa286864f864
Francois Suter <sutfra@gmail.com>
2021-11-18 15:34:08 +0100

    [TASK] Clean up SQL definition file

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,18 +2,8 @@
 # Table structure for table 'tx_externalimporttut_departments'
 #
 CREATE TABLE tx_externalimporttut_departments (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
 	code varchar(4) DEFAULT '' NOT NULL,
-	name varchar(255) DEFAULT '' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid)
+	name varchar(255) DEFAULT '' NOT NULL
 );
 
 
@@ -21,19 +11,9 @@ CREATE TABLE tx_externalimporttut_departments (
 # Table structure for table 'tx_externalimporttut_teams'
 #
 CREATE TABLE tx_externalimporttut_teams (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
 	code varchar(5) DEFAULT '' NOT NULL,
 	name varchar(255) DEFAULT '' NOT NULL,
-	members text,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid)
+	members text
 );
 
 
