d06d0301e2d3d7ab23a2eb72645e9486853ec7b3
Francois Suter <>
2024-12-08 21:19:18 +0100

    [TASK] Clean up with rector

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,12 +1,14 @@
 <?php
 
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
 return [
     'tx_externalimport_tut-department' => [
-        'provider' => TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => SvgIconProvider::class,
         'source' => 'EXT:externalimport_tut/Resources/Public/Icons/Department.svg'
     ],
     'tx_externalimport_tut-team' => [
-        'provider' => TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => SvgIconProvider::class,
         'source' => 'EXT:externalimport_tut/Resources/Public/Icons/Team.svg'
     ],
 ];
